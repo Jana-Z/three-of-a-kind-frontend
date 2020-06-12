c7862c38b4ce60696f7987cdbdbe72e5e64d781b
Jana-Z <github3@jana-zeller.de>
2020-06-12 21:54:03 +0200

    Deploy

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket('https://peaceful-brook-02371.herokuapp.com/');
 
 export const isConnected = (cb) => {
     socket.on("connected", () => cb(null));
